3d60607453c6a46c4b85d1fc4923bdcd1a88687a
tantaka0303 <hanshintigers.liked.2038@gmail.com>
2020-11-10 01:02:11 +0900

    DBの初期化処理の実装

---
+++ bin/main/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name) VALUES ('CPU');
+INSERT INTO users (name) VALUES ('ほんだ');
+INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Choki');
+INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Gu');
+INSERT INTO matches (user_1,user_2,user_1_hand,user_2_hand) VALUES (2,1,'Gu','Pa');
